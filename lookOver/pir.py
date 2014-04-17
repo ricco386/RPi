787cc8cdf59bca38e28e71e9abcfe8848093b848
Richard Kellner <richard.kellner@erigones.com>
2014-04-17 13:02:37 +0000

    Added variable with extra pin for LED Added counter of seconds between change of states

--- lookOver/pir.py
+++ lookOver/pir.py
@@ -9,48 +9,69 @@ class Sensor():
 
     current_motion = 0
     previous_motion = 0
+    GPIO_PIR = None
+    GPIO_LED = None
 
-    def __init__(self, pin_):
+    def __init__(self, pir_, led_):
         # Use BCM GPIO references
         # instead of physical pin numbers
         GPIO.setmode(GPIO.BCM)
 
-        if pin_ is None:
-            GPIO_PIR = pin_
+        print pir_
+        if pir_ is None:
+            self.GPIO_PIR = pir_
         else:
-            GPIO_PIR = 17
+            self.GPIO_PIR = 17
+
+        print led_
+        if led_ is None:
+            self.GPIO_LED = led_
+        else:
+            self.GPIO_LED = 7
 
         print "PIR Module Test (CTRL-C to exit)"
 
         # Set pin as input
-        GPIO.setup(GPIO_PIR,GPIO.IN)
+        GPIO.setup(self.GPIO_PIR, GPIO.IN)
+        # Set up pin for LED
+        GPIO.setup(self.GPIO_LED, GPIO.OUT)
 
-        while GPIO.input(GPIO_PIR)==1:
+        while GPIO.input(self.GPIO_PIR)==1:
             self.current_motion = 0
 
-    def sence():
+    def sense(self):
         print "Initiating PIR module"
-
+        counter = 0
         try:
 
             # Loop until users quits with CTRL-C
             while True:
 
                 # Read PIR state
-                self.current_motion = GPIO.input(GPIO_PIR)
+                self.current_motion = GPIO.input(self.GPIO_PIR)
 
                 if self.current_motion==1 and self.previous_motion==0:
                     # PIR is triggered
-                    print "  Motion detected!"
+                    print "  Motion detected! ("+ str(counter/100) +"s)"
+                    counter = 0
+
+                    # Turn on the LED
+                    GPIO.output(self.GPIO_LED, True)
                     # Record previous state
                     self.previous_motion=1
                 elif self.current_motion==0 and self.previous_motion==1:
                     # PIR has returned to ready state
-                    print "  Waiting for Motion"
+                    print "  Waiting for Motion ("+ str(counter/100) +"s)"
+                    counter = 0
+
+                    # Turn off the LED
+                    GPIO.output(self.GPIO_LED, False)
+                    # Record previous state
                     self.previous_motion=0
 
                 # Wait for 10 milliseconds
                 time.sleep(0.01)
+                counter += 1
 
         except KeyboardInterrupt:
             print "Quit"
