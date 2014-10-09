f8584da1387b9edec2cf43de516e51f66dd19ff9
Richard Kellner <richard.kellner@erigones.com>
2014-10-09 12:19:13 +0200

    Updated LED pin functionality to work with out LED as well

--- lookOver/pir.py
+++ lookOver/pir.py
@@ -13,6 +13,7 @@ from lookOver.out import Output
 
 class Sensor():
     args = None
+    cam = None
     prevState = False
     currState = False
     GPIO_PIR = None
@@ -21,11 +22,11 @@ class Sensor():
     def __init__(self, args):
         self.out = Output(args)
         self.args = args
+        self.cam = Camera(self.args)
         self.GPIO_PIR = 7
         if self.args.pir is not None:
             self.GPIO_PIR = self.args.pir
 
-        self.GPIO_LED = 17
         if self.args.led is not None:
             self.GPIO_LED = self.args.led
 
@@ -34,11 +35,25 @@ class Sensor():
         GPIO.setup(self.GPIO_PIR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self.out.msg('Setting pin %s for PIR sensor' % self.GPIO_PIR, DEBUG)
         # Set up pin for LED
-        # GPIO.setup(self.GPIO_LED, GPIO.OUT)
-        # self.out.msg('Setting pin for LED diode %s' % self.GPIO_LED, DEBUG)
+        if self.GPIO_LED:
+            GPIO.setup(self.GPIO_LED, GPIO.OUT)
+            self.out.msg('Setting pin for LED diode %s' % self.GPIO_LED, DEBUG)
+
+    def movement(self):
+        if self.GPIO_LED:
+            GPIO.output(self.GPIO_LED, True)
+            self.out.msg('Enabling LED diode at pin %s' % self.GPIO_LED, DEBUG)
+        if self.cam:
+            self.cam.start_recording()
+
+    def quiet(self):
+         if self.cam:
+            self.cam.stop_recording()
+         if self.GPIO_LED:
+            GPIO.output(self.GPIO_LED, False)
+            self.out.msg('Disabling LED diode at pin %s' % self.GPIO_LED, DEBUG)
 
     def sense(self):
-        cam = Camera(self.args)
 
         while True:
             time.sleep(0.1)
@@ -50,6 +65,6 @@ class Sensor():
                 self.out.msg("GPIO pin %s is %s" % (self.GPIO_PIR, newState), DEBUG)
 
                 if self.currState:
-                    cam.start_recording()
+                    self.movement()
                 else:
-                    cam.stop_recording()
+                    self.quiet()
