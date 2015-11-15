7c9ef752152ba6ac5b20649e601a7eca1e12c3ed
Richard Kellner <richard@kellnerovci.sk>
2015-11-15 22:54:08 +0100

    fixed errors when program was run in daemon mode

--- dht.py
+++ dht.py
@@ -7,16 +7,8 @@ import Adafruit_DHT
 import argparse
 from daemon3 import Daemon
 
-def setup_args():
-    ap = argparse.ArgumentParser(prog='DHTtemp')
-    ap.add_argument('-t', action='store_true', help='show temperature')
-    ap.add_argument('-hu', action='store_true', help='show humidity')
-    ap.add_argument('-p', type=int, help='set DHT sensor pin')
-    ap.add_argument('-pa', type=str, help='set path where log will be stored')
 
-    return ap.parse_args()
-
-class Dht():
+class Dht(object):
     SENSOR = Adafruit_DHT.DHT22
     PIN = 27
     TEMP = False
@@ -25,18 +17,18 @@ class Dht():
     def __init__(self, args=None):
         if args:
             if args.p:
-                PIN = args.p
+                self.PIN = args.p
             if args.t:
-                TEMP = args.t
+                self.TEMP = args.t
             if args.hu:
-                HUM = args.hu
+                self.HUM = args.hu
 
-    def sense():
+    def sense(self):
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-        retrun Adafruit_DHT.read_retry(SENSOR, PIN)
+        return Adafruit_DHT.read_retry(self.SENSOR, self.PIN)
 
-    def output():
+    def output(self):
         humidity, temperature = self.sense()
 
         # Note that sometimes you won't get a reading and the results will be null
@@ -44,16 +36,17 @@ class Dht():
         # If this happens try again!
         if humidity is not None and temperature is not None:
             out = ''
-            if TEMP:
+            if self.TEMP:
                 out += 'Temp={0:0.1f}*C '.format(temperature)
-            if HUM:
-                out += 'Humidity={1:0.1f}%'.format(humidity)
-            if not TEMP and not HUM:
-                out 'No value to read defined!'
+            if self.HUM:
+                out += 'Humidity={0:0.1f}%'.format(humidity)
+            if not self.TEMP and not self.HUM:
+                out = 'No value to read defined!'
         else:
             out = 'Failed to get reading. Try again!'
 
         return out
+
 
 class DhtDaemon(Daemon):
     LOG_PATH = '/tmp/dht.log'
@@ -62,9 +55,9 @@ class DhtDaemon(Daemon):
         self.args = args
 
         if args.pa:
-            LOG_PATH = args.pa
+            self.LOG_PATH = args.pa
 
-        logging.basicConfig(filename=LOG_PATH, level=logging.INFO)
+        logging.basicConfig(filename=self.LOG_PATH, level=logging.INFO)
         super(DhtDaemon, self).__init__(pidfile)
 
     def run(self):
@@ -73,6 +66,19 @@ class DhtDaemon(Daemon):
             logging.info(sensor.output())
             time.sleep(10)
 
+
+def setup_args():
+    ap = argparse.ArgumentParser(prog='DHTtemp')
+    ap.add_argument('-t', action='store_true', help='show temperature')
+    ap.add_argument('-hu', action='store_true', help='show humidity')
+    ap.add_argument('-p', type=int, help='set DHT sensor pin')
+    ap.add_argument('-pa', type=str, help='set path where log will be stored')
+    ap.add_argument('-d', type=str, help='daemon mode, usage: start|stop|restart')
+
+
+    return ap.parse_args()
+
+
 if __name__ == '__main__':
     args = setup_args()
 
@@ -80,7 +86,7 @@ if __name__ == '__main__':
         daemon = DhtDaemon('/tmp/dht.pid', args)
         if args.d in ('start', 'stop', 'restart'):
             if 'start' == args.d:
-                daemon.start(params)
+                daemon.start()
             elif 'stop' == args.d:
                 daemon.stop()
             elif 'restart' == args.d:
