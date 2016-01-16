2badefd5938301dc8561db5f7cc436f33d26dace
Richard Kellner <richard@kellnerovci.sk>
2016-01-16 02:00:27 +0100

    Moved doorman into separate class

--- doorman/main.py
+++ doorman/main.py
@@ -7,51 +7,7 @@
 import RPi.GPIO as GPIO
 import time
 import logging
-
-
-class Doorman():
-    sensor_pin = 16
-    sensor_state = 0
-    door_state = 2 # Set unknown value, due to initial reading
-    door_closed = 0
-    door_open = 1
-
-    def __init__(self):
-        # Prepare logging configuration
-        logconfig = {
-            'filename': '/tmp/doorman.log',
-            'level': logging.INFO,
-            'format': '%(asctime)s %(levelname)-8s %(name)s: %(message)s',
-        }
-        logging.basicConfig(**logconfig)
-        logging.info('Doorman is starting')
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.sensor_read()
-
-    def log_door_state(self):
-        if self.door_state == self.door_open:
-            logging.info('Door is open')
-        else:
-            logging.info('Door is closed')
-
-    def sensor_read(self):
-        self.sensor_state = GPIO.input(self.sensor_pin)
-
-        if self.sensor_state != self.door_state:
-            self.door_state = self.sensor_state
-            self.log_door_state()
-
-    def sense(self):
-        try:
-            while True:
-                self.sensor_read()
-                time.sleep(0.1)
-
-        except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-            GPIO.cleanup()
-
+from doorman.doorman import Doorman
 
 def start():
     d = Doorman()
