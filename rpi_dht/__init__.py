5d001e8e92d8d5a9d321386e4ef6b410cd8c7bc9
Richard Kellner <richard@kellnerovci.sk>
2016-04-10 18:29:20 +0200

    Updated version for sensor

--- rpi_dht/__init__.py
+++ rpi_dht/__init__.py
@@ -1,8 +1,8 @@
 """
 RPi.DHT
-Version: 0.1.0a
+Version: 0.1.0
 Homepage: https://github.com/ricco386/RPi-DHT
 See the LICENSE file for copying permission.
 """
 
-__version__ = '0.1.0a'
+__version__ = '0.1.0'
