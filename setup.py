4147ffb2e3eafa4b63595ea248a40af8c443e237
Richard Kellner <richard@kellnerovci.sk>
2016-04-10 18:27:53 +0200

    Updated version in setup file

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.rst", "r") as fp:
 
 setup(
     name="RPi.Sensor",
-    version="0.1a",
+    version="0.1.0",
     author="Richard Kellner",
     author_email="richard.kellner@gmail.com",
     url="https://github.com/ricco386/rpi-sensor",
