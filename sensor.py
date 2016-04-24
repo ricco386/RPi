51127754af723a911c4fc287388c0e18346428f2
Richard Kellner <richard@kellnerovci.sk>
2016-04-24 19:03:27 +0200

    Added ability to authenticate via username or token

--- sensor.py
+++ sensor.py
@@ -17,7 +17,11 @@ class Sensor(object):
     sensor_state = 0
     cycle_sleep = 0.1
     thread_exit = False
+
     url = False
+    token = False
+    username = False
+    password = False
 
     def __init__(self, args=[]):
         if args:
@@ -42,6 +46,15 @@ class Sensor(object):
         if hasattr(args, 'server') and args.server:
             self.url = args.server
             logging.info('%s will notify server: %s' % (self.NAME, self.url))
+
+        if hasattr(args, 'token') and args.token:
+            self.token = args.token
+            logging.debug('%s will authenticate on server via token.' % self.NAME)
+
+        if hasattr(args, 'username') and args.username and hasattr(args, 'password') and args.password:
+            self.username = args.username
+            self.password = args.password
+            logging.debug('%s will authenticate on server via username and password.' % self.NAME)
 
         if hasattr(args, 'pin') and args.pin:
             self.sensor_pin = args.pin
@@ -86,7 +99,15 @@ class Sensor(object):
         if not self.url:
             raise ValueError('Server has not been defined!')
 
-        r = requests.post(self.url, postdata)
+        if self.token:
+            headers = {
+                'Authorization': 'Token '+ self.token
+            }
+            r = requests.post(self.url, headers=headers, data=postdata)
+        elif self.username and self.password:
+            r = requests.post(self.url, auth(self.username, self.password), data=postdata)
+        else:
+            r = requests.post(self.url, data=postdata)
 
         if r.status_code == requests.codes.ok:
             logging.info("HTTP Post to %s was successful", self.url)
@@ -104,6 +125,9 @@ class Setup(object):
         ap.add_argument('-p', '--pin', type=int, help='Set sensor pin.')
         ap.add_argument('-l', '--log', type=str, help='Path where log will be stored.')
         ap.add_argument('-s', '--server', type=str, help='Server where data will be posted.')
+        ap.add_argument('-t', '--token', type=str, help='Security token for server authentication.')
+        ap.add_argument('--username', type=str, help='Username for server authentication.')
+        ap.add_argument('--password', type=str, help='Password for server authentication.')
         ap.add_argument('--debug', action='store_true', help='Store debug logs.')
 
         return ap
