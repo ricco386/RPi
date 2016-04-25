bde8206442327dd4ed392130acb4dead3809cd96
Richard Kellner <richard@kellnerovci.sk>
2016-04-25 21:26:32 +0200

    Slightly improoved way to post only data if they change

--- rpi_dht/dht.py
+++ rpi_dht/dht.py
@@ -11,31 +11,50 @@ class Dht(Sensor):
     SENSOR = Adafruit_DHT.DHT22
     NAME = 'DHT'
     sensor_pin = 21
+    cycle_sleep = 10
 
-    temperature = None
-    humidity = None
     data = None
+    nofity = True
+
+    def __init__(self, args=[]):
+        super(Dht, self).__init__(args)
+
+        self.data = {
+            'humidity': None,
+            'temperature': None,
+        }
 
     def __str__(self):
         self.output()
 
-    def sensor_read(self):
-        super(Dht, self).sensor_read()
+    def sensor_read_callback(self):
+        super(Dht, self).sensor_read_callback()
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
         humidity, temperature = Adafruit_DHT.read_retry(self.SENSOR, self.sensor_pin)
+        self.log.info('Data from sensor: %s *C and %s' % (temperature, humidity))
 
         if humidity is not None and temperature is not None:
-            self.data = {
-                'humidity': humidity,
-                'temperature': temperature,
-            }
+            if data['humidity'] != humidity:
+                data['humidity'] = humidity
+                self.post_data(self.generate_post_dict(self, humidity, 2))
 
-    def post_sensor_check(self):
-        super(Dht, self).post_sensor_check()
+            if data['temperature'] != temperature:
+                data['temperature'] = temperature
+                self.post_data((self.generate_post_dict(self, temperature, 1))
 
-        if self.url:
-            self.post_data(self.data)
+            self.failed = 0
+        else:
+            self.failed += 1
+
+    def generate_post_dict(self, value, unit):
+        # TODO: hardcoded values, BAD! Re-write in better way.
+        return {
+            'node': 1,
+            'sensor': 1,
+            'value': value,
+            'unit': unit,
+        }
 
     def output(self):
         # Note that sometimes you won't get a reading and the results will be null
