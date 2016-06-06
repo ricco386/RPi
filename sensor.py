5900d31ede074bb17492652990d361f130c3a6c5
Richard Kellner <richard@kellnerovci.sk>
2016-06-07 00:11:44 +0200

    Bug fix #3 Change argparse to config

--- sensor.py
+++ sensor.py
@@ -3,80 +3,84 @@
 #
 # This software is licensed as described in the README.rst and LICENSE files,
 # which you should have received as part of this distribution.
-import argparse
+import os
+import sys
 import RPi.GPIO as GPIO
-import time
+from utils import parse_loglevel
 import logging
-import requests
+try:
+    # noinspection PyCompatibility
+    from configparser import RawConfigParser
+except ImportError:
+    # noinspection PyCompatibility
+    from ConfigParser import RawConfigParser
 
 
-class Sensor(object):
+class SensorX(object):
 
-    NAME = 'sensor'
-    GPIO = GPIO
-    sensor_pin = 0
-    sensor_state = 0
-    cycle_sleep = 0
+    NAME = 'Sensor'
+    LOGFORMAT = '%(asctime)s %(levelname)-8s %(name)s: %(message)s'
+
     thread_exit = False
-
-    failed = 0
-    failed_notify = 5
-
-    log = None
-
-    url = False
-    token = False
-    username = False
-    password = False
+    cycle_sleep = False
 
     def __init__(self, args=[]):
-        if args:
-            self.args = args
+        self.logger = logging.getLogger(self.NAME)
+
+        cfg = 'sensor.cfg'
+        cfg_fp = None
+        cfg_lo = ((os.path.expanduser('~'), '.' + cfg), (sys.prefix, 'etc', cfg), ('/etc', cfg))
+        config_base_sections = ('global',)
+
+        # Try to read config file from ~/.sensor.cfg or /etc/sensor.cfg
+        for i in cfg_lo:
+            try:
+                cfg_fp = open(os.path.join(*i))
+            except IOError:
+                continue
+            else:
+                break
+
+        if not cfg_fp:
+            sys.stderr.write("""\nSensor can't start!\n
+You need to create a config file in one these locations: \n%s\n
+You can rename sensor.cfg.example and update the required options.
+The example file is located in: %s\n\n""" % (
+    '\n'.join([os.path.join(*i) for i in cfg_lo]),
+    os.path.dirname(os.path.abspath(__file__))))
+            sys.exit(1)
+
+        # Read and parse configuration
+        # noinspection PyShadowingNames
+        def load_config(fp, reopen=False):
+            config = RawConfigParser()
+            if reopen:
+                fp = open(fp.name)
+            config.readfp(fp)  # TODO: Deprecated since python 3.2
+            fp.close()
+            return config
+        self.config = load_config(cfg_fp)
 
         # Prepare logging configuration
-        self.set_logging()
-        self.log.info('%s is at your service...' % self.NAME)
-
-        if hasattr(args, 'server') and args.server:
-            self.url = args.server
-            self.log.info('Notify server: %s' % self.url)
-
-        if hasattr(args, 'token') and args.token:
-            self.token = args.token
-            self.log.info('Authenticate on server via token.')
-
-        if hasattr(args, 'username') and args.username and hasattr(args, 'password') and args.password:
-            self.username = args.username
-            self.password = args.password
-            self.log.info('Authenticate on server via username and password.')
-
-        if hasattr(args, 'pin') and args.pin:
-            self.sensor_pin = args.pin
-        elif self.sensor_pin < 0 or self.sensor_pin > 40:
-            self.log.error('PIN have to between 1 and 40')
-            raise Exception
-
-        self.sensor_setup()
-
-    def set_logging(self):
         logconfig = {
-            'filename': '/tmp/%s.log' % self.NAME,
-            'level': logging.INFO,
-            'format': '%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+            'level': parse_loglevel(self.config.get('global', 'loglevel')),
+            'format': self.LOGFORMAT,
         }
 
-        if hasattr(self.args, 'log') and self.args.log:
-            logconfig['filename'] = self.args.log
+        if self.config.has_option('global', 'logfile'):
+            logfile = self.config.get('global', 'logfile').strip()
+            if logfile:
+                logconfig['filename'] = logfile
 
-        if hasattr(self.args, 'debug') and self.args.debug:
-            logconfig['level'] = logging.DEBUG
-
+        # Setup logging
         logging.basicConfig(**logconfig)
-        self.log = logging.getLogger(self.NAME)
+        self.logger.info('Loaded configuration from %s', cfg_fp.name)
 
     def sensor_setup(self):
-        self.log.debug('Initial sensor setup.')
-        self.log.info('Sensor at PIN: %s' % self.sensor_pin)
+        self.logger.debug('Initial sensor setup.')
+
+        self.sensor_pin =  self.config.get('global', 'sensor_pin')
+        self.logger.info('Sensor at PIN: %s' % self.sensor_pin)
         self.set_gpio()
 
     def sensor_cleanup(self):
@@ -86,83 +90,80 @@ class Sensor(object):
         self.GPIO.setmode(GPIO.BOARD)
         self.GPIO.setup(self.sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def pre_sensor_read_callback(self):
-        self.log.debug('Pre-read sensor callback.')
+    def failed_notification(self):
+        if self.config.has_option('global', 'failed_notify') and \
+                self.failed >= self.config.get('global', 'failed_notify'):
+            self.logger.warning('Sensor reading has failed %s in a row.' % self.failed)
 
-    def post_sensor_read_callback(self):
-        self.log.debug('Post-read sensor callback.')
-        self.failed_notification()
+    def pre_sensor_read_callback(self):
+        self.logger.debug('Pre-read sensor callback.')
 
     def sensor_read_callback(self):
-        self.log.debug('Sensor read callback')
+        self.logger.debug('Sensor read callback.')
+
+    def post_sensor_read_callback(self):
+        self.logger.debug('Post-read sensor callback.')
+        self.failed_notification()
 
     def sensor_read(self):
         self.pre_sensor_read_callback()
         self.sensor_read_callback()
         self.post_sensor_read_callback()
 
-    def failed_notification(self):
-        if self.failed >= self.failed_notify:
-            self.log.warning('Sensor reading has failed %s in a row.' % self.failed)
-
     def sense(self):
         try:
-            self.log.debug('Starting permanent sensing process...')
+            self.logger.debug('Starting permanent sensing process...')
+            self.sensor_setup()
 
             while not self.thread_exit:
                 self.sensor_read()
-                time.sleep(self.cycle_sleep)
+
+                if self.cycle_sleep:
+                    time.sleep(self.cycle_sleep)
 
                 if self.thread_exit:
-                    self.log.info('Signal send to interupted sensing process...')
+                    self.logger.info('Signal send to interupted sensing process...')
 
         except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-            self.log.info('You have interupted sensing process...')
+            self.logger.info('You have interupted sensing process...')
 
         self.sensor_cleanup()
 
-    def post_data(self, postdata):
-        if not self.url:
-            self.log.error('Trying to post data, but no server bas been defined!')
-            raise ValueError('Server has not been defined!')
 
-        r = requests.get(self.url)
+    def post_data(self, path, postdata):
+        if not self.config.has_option('server', 'hostname'):
+            self.logger.error('Trying to post data, but no server bas been defined!')
+            raise ValueError('Server has not been defined!')
+        else:
+            url = '%s/%s' % (self.config.has_option('server', 'hostname'), path)
+
+        r = requests.get(url)
         if not self.verify_server_response(r):
             return False
 
-        if self.token:
+        if not self.config.has_option('server', 'token'):
             headers = {
-                'Authorization': 'Token '+ self.token
+                'Authorization': 'Token %s' % (self.config.get('server', 'token'),)
             }
-            r = requests.post(self.url, headers=headers, data=postdata)
+            r = requests.post(url, headers=headers, data=postdata)
         elif self.username and self.password:
-            r = requests.post(self.url, auth=(self.username, self.password), data=postdata)
+            r = requests.post(url, auth=(self.config.get('server', 'username'), self.config.get('server', 'password')), data=postdata)
         else:
-            r = requests.post(self.url, data=postdata)
+            r = requests.post(url, data=postdata)
 
         return self.verify_server_response(r)
 
-
     def verify_server_response(self, r):
         if r.status_code in (200, 201, 202, 203, 204, 205, 206):
-            self.log.info("HTTP %s response %s: %s to %s was successful" % (r.request.method, r.status_code, r.reason, self.url))
+            self.logger.info("SUCCESSFUL HTTP %s response %s: %s." % (r.request.method, r.status_code, r.reason))
             return True
 
         else:
-            self.log.error("HTTP %s response %s: %s to %s has failed..." % (r.request.method, r.status_code, r.reason, self.url))
+            self.logger.error("FAILED HTTP %s response %s: %s." % (r.request.method, r.status_code, r.reason))
             return False
 
 
-class Setup(object):
+if __name__ == "__main__":
+    # execute only if run as a script
+    SensorX()
 
-    def args(self, name, desc):
-        ap = argparse.ArgumentParser(prog=name, description=desc)
-        ap.add_argument('-p', '--pin', type=int, help='Set sensor pin.')
-        ap.add_argument('-l', '--log', type=str, help='Path where log will be stored.')
-        ap.add_argument('-s', '--server', type=str, help='Server where data will be posted.')
-        ap.add_argument('-t', '--token', type=str, help='Security token for server authentication.')
-        ap.add_argument('--username', type=str, help='Username for server authentication.')
-        ap.add_argument('--password', type=str, help='Password for server authentication.')
-        ap.add_argument('--debug', action='store_true', help='Store debug logs.')
-
-        return ap
