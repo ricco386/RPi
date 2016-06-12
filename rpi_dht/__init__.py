9bd816e76110528a19f530ecf8c31419ec40de90
Richard Kellner <richard@kellnerovci.sk>
2016-06-13 00:12:14 +0200

    Updated new development version

--- rpi_dht/__init__.py
+++ rpi_dht/__init__.py
@@ -1,8 +1,8 @@
 """
 RPi.DHT
-Version: 0.2.2
+Version: 0.2.3
 Homepage: https://github.com/ricco386/RPi-DHT
 See the LICENSE file for copying permission.
 """
 
-__version__ = '0.2.2'
+__version__ = '0.2.3a'
