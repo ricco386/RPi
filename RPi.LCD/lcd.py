b27ebdd45a39e57e0cb137ab48b17a313abd50cb
Richard Kellner <richard.kellner@gmail.com>
2017-12-10 12:38:27 +0100

    Moved RPi.LCD to separate repository
    This was done due to multiple repo github.com:ricco386/RPi.LCD.git
    merge and clean up so all my RPi projects are easily accessible
    and also maintainable.

--- RPi.LCD/lcd.py
+++ RPi.LCD/lcd.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# This software is licensed as described in the README.rst and LICENSE files,
-# which you should have received as part of this distribution.
+#!/usr/bin/python
+import sys
 import threading
 from clock import Clock
+#from tail import tail
+import time
 
 from pylcdsysinfo import LCDSysInfo, TextLines, BackgroundColours, TextColours
 
@@ -28,13 +27,25 @@ class LCD():
             while True:
                 self.display_clock(self.c.time)
 
+#                self.d.display_text_on_line(4, 'aaaaaaa', False, None, TextColours.GREEN)
+#                time.sleep(2)
+#                self.d.clear_lines(TextLines.LINE_4, self.bg)
+#                time.sleep(2)
+#                for i in range(0, tail_rows):
+#                    d.display_text_on_line(6-i, t.p[i], False, None, i)
+
         except KeyboardInterrupt:
             self.c.exit = True
             self.d.display_text_on_line(3, 'Bye!', False, 0, TextColours.PINK)
+            #t.exit = True
 
     def start_tail_thread(self):
         self.t = tail(tail_rows)
         threading.Thread(target=self.t.run).start()
+
+    def start_clock_thread(self):
+        self.c = Clock()
+        threading.Thread(target=self.c.run).start()
 
     def display_clock(self, time):
         self.d.display_text_on_line(1, time[11:19], False, None, TextColours.GREEN)
@@ -45,3 +56,5 @@ class LCD():
 if __name__ == "__main__":
     lcd = LCD()
     lcd.run()
+
+    sys.exit(0)
