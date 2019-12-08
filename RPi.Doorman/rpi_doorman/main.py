fa2e59dae18fcb480752611fa4f54a5953e63920
Richard Kellner <richard.kellner@gmail.com>
2019-12-08 14:38:36 +0100

    Updated Magnetic Sensor to support refactored RPi.Sensor base class

--- RPi.Doorman/rpi_doorman/main.py
+++ RPi.Doorman/rpi_doorman/main.py
@@ -12,20 +12,34 @@ def setup_args():
     ap = argparse.ArgumentParser(prog='doorman',
             description='RPi.Doorman is using adafruit magnetic contact switch (door sensor), will permanently sense for door status and its changes.'
             )
-    ap.add_argument('-c', action='store_true', help='Current door status will be shown.')
-    ap.add_argument('--notify', type=str, help='Jabber ID to notify via Ludolph webhook with sensor state change (POST).')
+    ap.add_argument('-s', '--status', action='store_true', help='Current door status will be shown.')
     ap.add_argument('-p', '--pin', type=int, help='Pin number, for GPIO magnetic contact switch (door sensor).')
-    ap.add_argument('-l', '--log', type=str, help='Log path, where logs will be stored.')
-    ap.add_argument('-d', '--debug', action='store_true', help='Debug mode, wheather store debug info in log.')
+    ap.add_argument('--failed_notify', type=int, help='Number of failed sensor reading before alerting.')
+    ap.add_argument('--cycle_sleep', type=int, help='Number of failed sensor reading before alerting.')
 
     return ap.parse_args()
 
 
 def main():
+    d = Doorman()
     args = setup_args()
-    d = Doorman(args)
 
-    if hasattr(args, 'c') and args.c:
+    if hasattr(args, 'pin') and args.pin:
+        d.PIN = args.pin
+        d.logger.info('Sensor %s at PIN: %s (set by script parameter, overwriting configuration value).', d.NAME, d.PIN)
+
+    if hasattr(args, 'failed_notify') and args.failed_notify:
+        d.FAILED_NOTIF = args.failed_notify
+        d.logger.debug('Sensor %s at failed_notify: %s (set by script parameter, overwriting configuration value).',
+                       d.NAME, d.FAILED_NOTIF)
+
+    if hasattr(args, 'cycle_sleep') and args.cycle_sleep:
+        d.SLEEP = args.cycle_sleep
+        d.logger.debug('Sensor %s at cycle_sleep: %s (set by script parameter, overwriting configuration value).',
+                       d.NAME, d.SLEEP)
+
+    if hasattr(args, 'status') and args.status:
+        d.sensor_read()
         print(d.get_door_state())
     else:
         d.sense()
--- RPi.Doorman/rpi_doorman/rpi_doorman.py
+++ RPi.Doorman/rpi_doorman/rpi_doorman.py
@@ -3,95 +3,29 @@
 #
 # This software is licensed as described in the README.rst and LICENSE files,
 # which you should have received as part of this distribution.
-
-import RPi.GPIO as GPIO
-import time
-import logging
+from sensor import Sensor
 
 
-class Doorman(object):
-    args = []
-    sensor_pin = 16
+class Doorman(Sensor):
+
+    NAME = 'Magnetic_Contact'
+    PIN = 16
+
     sensor_state = 0
-    door_state = 2 # Set unknown value, due to initial reading
+    door_state = 2  # Set unknown value, due to initial reading
     door_closed = 0
     door_open = 1
 
-    def __init__(self, args=[]):
-        if args:
-            self.args = args
-
-        # Prepare logging configuration
-        logconfig = {
-            'filename': '/tmp/doorman.log',
-            'level': logging.INFO,
-            'format': '%(asctime)s %(levelname)-8s %(name)s: %(message)s',
-        }
-
-        if hasattr(args, 'log') and args.log:
-            logconfig['filename'] = args.log
-
-        if hasattr(args, 'debug') and args.debug:
-            logconfig['level'] = logging.DEBUG
-
-        logging.basicConfig(**logconfig)
-        logging.info('Doorman is at your service')
-
-        if hasattr(args, 'pin') and args.pin:
-            self.sensor_pin = args.pin
-        logging.debug('Sensor PIN: %s', self.sensor_pin)
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.sensor_read()
-
     def get_door_state(self):
         if self.door_state == self.door_open:
-            return 'Door is open'
+            return 'Magnetic contact is open'
         else:
-            return 'Door is closed'
+            return 'Magnetic contact is closed'
 
-    def notify(self):
-        if hasattr(self.args, 'notify') and self.args.notify:
-            try:
-                import requests
-            except ImportError:
-                logger.error('Can not import requests library. Make sure it is installed!')
-            else:
-                r = requests.post("http://127.0.0.1:8922/doorman_update",
-                        data = {
-                            "user": self.args.notify,
-                            "msg": self.get_door_state()
-                            }
-                        )
-                log = 'Notification response code: %s (user: %s, msg: %s)' % (r.status_code, self.args.notify,
-                        self.get_door_state())
-
-                if r.status_code != requests.codes.ok:
-                    logging.error(log)
-                else:
-                    logging.debug(log)
-
-    def callback_sensor_read(self):
-        logging.info(self.get_door_state())
-
-    def sensor_read(self):
-        self.sensor_state = GPIO.input(self.sensor_pin)
+    def sensor_read_callback(self):
+        self.sensor_state = 1  # self.GPIO.input(self.PIN)
 
         if self.sensor_state != self.door_state:
-            logging.debug('Changing sensor state from %s to %s' % (self.door_state, self.sensor_state))
+            self.logger.debug('Changing magnetic contact state from %s to %s', self.door_state, self.sensor_state)
             self.door_state = self.sensor_state
-            self.callback_sensor_read()
-            self.notify()
-
-    def sense(self):
-        try:
-            logging.debug('Doorman is starting to watch the door status')
-            while True:
-                self.sensor_read()
-                time.sleep(0.1)
-
-        except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-            logging.info('Thank you for using my service, your Doorman!')
-            GPIO.cleanup()
-
+            self.logger.info(self.get_door_state())
