43bffabc3884a8ab3a3f4ade826e568842c1f066
Richard Kellner <richard.kellner@gmail.com>
2019-12-08 14:38:11 +0100

    Updated base sensor to have  ability to overwrite the configuration

--- RPi.Sensor/sensor.py
+++ RPi.Sensor/sensor.py
@@ -9,17 +9,18 @@ import signal
 
 import RPi.GPIO as GPIO
 
-from .utils import parse_loglevel, init_config_file
+from utils import parse_loglevel, init_config_file
 
 
 class Sensor(object):
 
     NAME = 'Sensor'
     PIN = None
-    SLEEP = False
-    EXIT = False
+    GPIO = None
     FAILED = 0
     FAILED_NOTIF = 10
+    SLEEP = 0
+    EXIT = False
 
     def __init__(self):
         self.logger = logging.getLogger(self.NAME)
@@ -36,6 +37,8 @@ class Sensor(object):
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
 
+        self.sensor_setup()
+
     def exit_gracefully(self, signum, frame):
         self.EXIT = True
         self.logger.info('Sensor %s received interrupt signal.', self.NAME)
@@ -45,33 +48,36 @@ class Sensor(object):
         Initial function to configure sensor before the main infinite loop
         """
         self.logger.debug('Sensor %s initial setup.', self.NAME)
+        cycle_sleep = False
+        sensor_failed_notif = False
 
-        self.PIN = self.config.get(self.NAME, 'sensor_pin')
-        self.logger.info('Sensor %s at PIN: %s.', self.NAME, self.PIN)
+        if self.NAME in self.config:
+            self.PIN = self.config.get(self.NAME, 'sensor_pin', fallback=self.PIN)
+            cycle_sleep = int(self.config.get(self.NAME, 'cycle_sleep', fallback=0))
+            sensor_failed_notif = int(self.config.get(self.NAME, 'failed_notify', fallback=0))
 
-        self.SLEEP = self.config.get(self.NAME, 'cycle_sleep', fallback=False)
-        self.logger.info('Sensor %s at cycle sleep: %s.', self.NAME, self.SLEEP)
-
-        sensor_failed_notif = self.config.get(self.NAME, 'failed_notify', fallback=False)
-
-        if not sensor_failed_notif:
-            self.FAILED_NOTIF = self.config.get('global', 'failed_notify', fallback=10)
+        if cycle_sleep:
+            self.SLEEP = cycle_sleep
         else:
+            self.SLEEP = int(self.config.get('global', 'cycle_sleep', fallback=0))
+
+        if sensor_failed_notif:
             self.FAILED_NOTIF = sensor_failed_notif
+        else:
+            self.FAILED_NOTIF = int(self.config.get('global', 'failed_notify', fallback=10))
 
-        self.config.get('global', 'failed_notify', fallback=10)
-
-        self.gpio_setup()
-
-    def gpio_cleanup(self):
-        self.GPIO.cleanup()
-        self.logger.debug('Sensor %s GPIO cleanup.', self.NAME)
+        self.logger.debug('Sensor %s at cycle_sleep: %s.', self.NAME, self.SLEEP)
+        self.logger.debug('Sensor %s at failed_notify: %s.', self.NAME, self.FAILED_NOTIF)
 
     def gpio_setup(self):
         self.GPIO = GPIO
         self.GPIO.setmode(GPIO.BOARD)
         self.GPIO.setup(self.PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.logger.debug('Sensor %s GPIO set.', self.NAME)
+        self.logger.info('Sensor %s at PIN: %s.', self.NAME, self.PIN)
+
+    def gpio_cleanup(self):
+        self.GPIO.cleanup()
+        self.logger.debug('Sensor %s GPIO cleanup.', self.NAME)
 
     def failed_notification_callback(self):
         pass
@@ -81,6 +87,9 @@ class Sensor(object):
         Helper function with code to be run before reading the sensor
         """
         self.logger.debug('Pre-read sensor callback.')
+
+        if self.GPIO is None:
+            self.gpio_setup()
 
     def sensor_read_callback(self):
         """
@@ -107,21 +116,13 @@ class Sensor(object):
         """
         Main sensor function to run!
         """
-        if self.NAME == 'Sensor':
-            raise NotImplementedError('Sensor NAME has to be defined!')
-
         self.logger.debug('Sensor %s starting permanent sensing process...', self.NAME)
-        self.sensor_setup()
 
-        while not self.thread_exit:
-            try:
-                self.sensor_read()
+        while not self.EXIT:
+            self.sensor_read()
 
-                if self.SLEEP:
-                    time.sleep(self.SLEEP)
-
-            except KeyboardInterrupt:  # If CTRL+C is pressed, exit cleanly:
-                self.EXIT = True
-                self.logger.info('Sensor %s you have interrupted sensing process...', self.NAME)
+            if self.SLEEP:
+                time.sleep(self.SLEEP)
 
         self.gpio_cleanup()
+        self.logger.info('Sensor %s has correctly finished sensing... BYE!', self.NAME)
