32d2a50cb729f5f527b1914e68651269e8d36929
Richard Kellner <richard.kellner@gmail.com>
2020-01-03 21:49:20 +0100

    Added ability to dealy sensor reportings

--- RPi.PIR/rpi_pir/main.py
+++ RPi.PIR/rpi_pir/main.py
@@ -15,6 +15,7 @@ def setup_args():
     ap.add_argument('--gpio_bcm', action='store_true', help='Switch PIN to GPIO BCM numbers.')
     ap.add_argument('--failed_notify', type=int, help='Number of failed sensor reading before alerting.')
     ap.add_argument('--cycle_sleep', type=float, help='Sleep time in the loop to slow down sensor readings.')
+    ap.add_argument('--change_delay', type=int, help='Delay in seconds to report state change.')
 
     return ap.parse_args()
 
@@ -27,6 +28,10 @@ def main():
         name = params.name
 
     pir = PIR(name=name, params=params)
+
+    if hasattr(params, 'change_delay') and params.change_delay:
+        pir.change_delay = params.change_delay
+
     pir.sense()
 
 
--- RPi.PIR/rpi_pir/rpi_pir.py
+++ RPi.PIR/rpi_pir/rpi_pir.py
@@ -3,6 +3,7 @@
 #
 # This software is licensed as described in the README.rst and LICENSE files,
 # which you should have received as part of this distribution.
+import time
 from sensor import Sensor
 
 
@@ -13,22 +14,37 @@ class PIR(Sensor):
 
     sensor_state = 0
     previous_state = 0
+    change_timestamp = 0.0
+    change_delay = 30
 
     def settledown(self):
         self.logger.debug('Sensor %s waiting to settle down ...', self.NAME)
         # Loop until PIR output is 0
-        while self.GPIO.input(self.PIN)==1:
+        while self.GPIO.input(self.PIN) == 1:
             self.sensor_state = 0
 
-        self.logger.debug('... Ready!')
+        self.logger.info('Sensor %s state is LOW.', self.NAME)
 
     def gpio_setup(self, gpio_bcm=False):
         super().gpio_setup(gpio_bcm)
         self.settledown()
 
-    def sensor_read_callback(self):
+    def pre_sensor_read_callback(self):
+        super().pre_sensor_read_callback()
+        # Record current sensor state for future comparasion
         self.previous_state = self.sensor_state
+
+    def sensor_read_callback(self):
+        # Read current sensor state
         self.sensor_state = self.GPIO.input(self.PIN)
 
+    def post_sensor_read_callback(self):
         if self.sensor_state != self.previous_state:
-            self.logger.info('Sensor %s state is %s.', self.NAME, "HIGH" if self.sensor_state else "LOW")
+            if self.change_timestamp < time.time() - self.change_delay:
+                # Sensor change state time delay has passed, lets alert an event
+                self.logger.info('Sensor %s state is %s.', self.NAME, "HIGH" if self.sensor_state else "LOW")
+            else:
+                self.change_timestamp = time.time()  # Reset the timestamp in order 
+                self.sensor_state = self.previous_state  # Return to the state before reading
+
+        super().post_sensor_read_callback()
